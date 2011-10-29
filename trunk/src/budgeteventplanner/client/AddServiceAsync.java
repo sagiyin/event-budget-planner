598530f027c5e7937053209f26f9eeecbbd782dd
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-10-29 20:05:44 +0000

    Remove AddService Example

--- trunk/src/budgeteventplanner/client/AddServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package budgeteventplanner.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface AddServiceAsync {
-	void add(Integer a, Integer b, AsyncCallback<String> callback);
-}
