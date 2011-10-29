e6c8162f6bcc689be832f139ebbec158d1cee464
xiayuan0623@gmail.com <xiayuan0623@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-10-29 14:55:47 +0000

    Yuan's first commit

--- src/budgeteventplanner/client/BudgetEventPlanner.java
+++ src/budgeteventplanner/client/BudgetEventPlanner.java
@@ -41,7 +41,7 @@ public class BudgetEventPlanner implements EntryPoint {
 	public void onModuleLoad() {
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		nameField.setText("CS Yuan");
 		final Label errorLabel = new Label();
 
 		// We can add style names to widgets
