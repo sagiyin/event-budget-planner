fd6298c9da60a15c3eba9e19b07fc906aee8c0a0
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-10-29 20:05:44 +0000

    Remove AddService Example

--- src/budgeteventplanner/client/AddService.java
+++
@@ -1,9 +0,0 @@
-package budgeteventplanner.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("add")
-public interface AddService extends RemoteService {
-	String add(Integer a, Integer b);
-}
