cd2e14d9a07b768dbc64c438ccf24940ddcf4e07
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-16 02:22:59 +0000

    Implement AttendeeManager Database Interface

--- src/budgeteventplanner/client/AttendeeRegistration.java
+++ src/budgeteventplanner/client/AttendeeRegistration.java
@@ -6,6 +6,7 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ButtonBase;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
@@ -27,9 +28,35 @@ public class AttendeeRegistration implements EntryPoint {
 	 */
 	private final GreetingServiceAsync greetingService = GWT
 			.create(GreetingService.class);
-	
-	
-	
+	final Label emailLabel=new Label("*Email:");
+	final TextBox emailBox=new TextBox();
+	final Label firstNameLabel=new Label("*First Name:");
+	final TextBox firstNameBox=new TextBox();
+	final Label midNameLabel=new Label("Mid Name:");
+	final TextBox midNameBox=new TextBox();
+	final Label lastNameLabel=new Label("*Last Name:");
+	final TextBox lastNameBox=new TextBox();
+	final Label jobTitleLabel=new Label("Job Title:");
+	final TextBox jobTitleBox=new TextBox();
+	final Label companyLabel=new Label("Company:");
+	final TextBox companyBox=new TextBox();
+	final Label address1Label=new Label("*Address 1:");
+	final TextBox address1Box=new TextBox();
+	final Label address2Label=new Label("Address 2:");
+	final TextBox address2Box=new TextBox();
+	final Label cityLabel=new Label("*City:");
+	final TextBox cityBox=new TextBox();
+	final Label stateLabel=new Label("*State:");
+	final TextBox stateBox=new TextBox();
+	final Label zipLabel=new Label("*Zip:");
+	final TextBox zipBox=new TextBox();
+	final Label phoneLabel=new Label("*Phone Number:");
+	final TextBox phoneBox=new TextBox();
+	final Button submit=new Button("submit");
+	final DialogBox dialogBox=new DialogBox();
+	final Button closeButton = new Button("Close");
+	VerticalPanel dialogVPanel = new VerticalPanel();
+	private static String attendeeID=null;
 	/**
 	 * This is the entry point method.
 	 */
@@ -37,75 +64,63 @@ public class AttendeeRegistration implements EntryPoint {
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		final Label emailLabel=new Label("*Email:");
-		final TextBox emailBox=new TextBox();
+		
 		RootPanel.get("email").add(emailLabel);
 		RootPanel.get("emailBox").add(emailBox);
 		
-		final Label firstNameLabel=new Label("*First Name:");
-		final TextBox firstNameBox=new TextBox();
+		
 		RootPanel.get("firstName").add(firstNameLabel);
 		RootPanel.get("firstNameBox").add(firstNameBox);
 	
-		final Label midNameLabel=new Label("Mid Name:");
-		final TextBox midNameBox=new TextBox();
+
 		RootPanel.get("midName").add(midNameLabel);
 		RootPanel.get("midNameBox").add(midNameBox);
 		
-		final Label lastNameLabel=new Label("*Last Name:");
-		final TextBox lastNameBox=new TextBox();
+
 		RootPanel.get("lastName").add(lastNameLabel);
 		RootPanel.get("lastNameBox").add(lastNameBox);
 		
 		
-		final Label jobTitleLabel=new Label("Job Title:");
-		final TextBox jobTitleBox=new TextBox();
+
 		RootPanel.get("jobTitle").add(jobTitleLabel);
 		RootPanel.get("jobTitleBox").add(jobTitleBox);
 		
-		final Label companyLabel=new Label("Company:");
-		final TextBox companyBox=new TextBox();
+
 		RootPanel.get("company").add(companyLabel);
 		RootPanel.get("companyBox").add(companyBox);
 		
-		final Label address1Label=new Label("*Address 1:");
-		final TextBox address1Box=new TextBox();
+
 		RootPanel.get("address1").add(address1Label);
 		RootPanel.get("address1Box").add(address1Box);
 		
-		final Label address2Label=new Label("Address 2:");
-		final TextBox address2Box=new TextBox();
+
 		RootPanel.get("address2").add(address2Label);
 		RootPanel.get("address2Box").add(address2Box);
 		
-		final Label cityLabel=new Label("*City:");
-		final TextBox cityBox=new TextBox();
+
 		RootPanel.get("city").add(cityLabel);
 		RootPanel.get("cityBox").add(cityBox);
 		
-		final Label stateLabel=new Label("*State:");
-		final TextBox stateBox=new TextBox();
+
 		RootPanel.get("state").add(stateLabel);
 		RootPanel.get("stateBox").add(stateBox);
 		
-		final Label zipLabel=new Label("*Zip:");
-		final TextBox zipBox=new TextBox();
+
 		RootPanel.get("zip").add(zipLabel);
 		RootPanel.get("zipBox").add(zipBox);
 		
-		final Label phoneLabel=new Label("*Phone Number:");
-		final TextBox phoneBox=new TextBox();
+
 		RootPanel.get("phone").add(phoneLabel);
 		RootPanel.get("phoneBox").add(phoneBox);
 		
-		final Button submit=new Button("submit");
+	
 		RootPanel.get("submit").add(submit);
 		//dialog box
-		final DialogBox dialogBox=new DialogBox();
-		final Button closeButton = new Button("Close");
-		VerticalPanel dialogVPanel = new VerticalPanel();
+
+		
 		dialogVPanel.addStyleName("dialogVPanel");
 		dialogVPanel.add(new HTML("<b>Submit Successful!</b>"));
+	
 		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
 		dialogVPanel.add(closeButton);
 		dialogBox.setWidget(dialogVPanel);
@@ -120,17 +135,21 @@ public class AttendeeRegistration implements EntryPoint {
 		});   
 		//emailBox.setFocus(true);
 		
+	
 	        class MyHandler implements ClickHandler {
 		
 			
 				public void onClick(ClickEvent event) {
+					
+					if(!checkForm())	return;
+					
 					greetingService.sendEmail(firstNameBox.getText(), emailBox.getText(), new AsyncCallback<Integer>(){
 						public void onFailure(Throwable caught) {
 						}
 
 						public void onSuccess(Integer result) {
 							
-							
+							((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Submit Successful!</b>");
 							dialogBox.center();
 							closeButton.setFocus(true);
 							//RootPanel.get("attendeeManager").setVisible(true);
@@ -157,6 +176,62 @@ public class AttendeeRegistration implements EntryPoint {
 	        }
 	        MyHandler handler = new MyHandler();
 	        submit.addClickHandler(handler);
+	}
+	
+	public static void setAttendeeID(String attendeeIDc){
+		attendeeID=attendeeIDc;
+	} 
+	boolean checkForm(){
+		if(emailBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your email address</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(!(emailBox.getText().contains("@") &&emailBox.getText().contains("."))){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input a legal email address</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(firstNameBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your first name</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(address1Box.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your address</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(cityBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the city you live in</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(stateBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the state you live in</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(zipBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the ZIP code</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		if(phoneBox.getText().compareTo("")==0 ){
+			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your phone number</b>");
+			dialogBox.center();
+			closeButton.setFocus(true);
+			return false;
+		}
+		
+		 return true;
 	}
 
 }
