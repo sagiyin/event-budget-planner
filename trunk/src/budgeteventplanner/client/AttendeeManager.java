98f51e5d9ed1501d394013199ccfbfee42da55a6
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-16 02:22:59 +0000

    Implement AttendeeManager Database Interface

--- trunk/src/budgeteventplanner/client/AttendeeManager.java
+++ trunk/src/budgeteventplanner/client/AttendeeManager.java
@@ -1,5 +1,14 @@
+/*
+ * 1 needs to find the attendeetList from server though event name
+ */
+
 package budgeteventplanner.client;
 
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import budgeteventplanner.client.entity.Attendee;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
@@ -12,6 +21,7 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Tree;
 import com.google.gwt.user.client.ui.TreeItem;
+import com.ibm.icu.util.StringTokenizer;
 
 public class AttendeeManager implements EntryPoint {
 
@@ -19,79 +29,98 @@ public class AttendeeManager implements EntryPoint {
 	 * The message displayed to the user when the server cannot be reached or
 	 * returns an error.
 	 */
+	@SuppressWarnings("unused")
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
 
 	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
 	 */
-	private final GreetingServiceAsync greetingService = GWT
+	@SuppressWarnings("unused")
+	private final static AttendeeServiceAsync attendeeService = GWT
 			.create(GreetingService.class);
-	
-	
+
+	final static TextArea infoBox = new TextArea();
+	static MyLabel[] attendees;// = new Label[5];
+	static HorizontalPanel hPanel = new HorizontalPanel();
+	static TreeItem root = new TreeItem("Attendees");
+	List<Attendee> attendeeList;
 	@Override
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
-		
-		
-		HorizontalPanel hPanel=new HorizontalPanel();
+
 		hPanel.setSpacing(50);
 		RootPanel.get("attendees").add(hPanel);
 		hPanel.setSize("100%", "100%");
-		
-
-		
-		final TextArea infoBox=new TextArea();
-		infoBox.setSize("500px","500px");
+		infoBox.setSize("500px", "500px");
 		infoBox.setReadOnly(true);
 		infoBox.setText("here");
-		
-		//TreeItem hTree =new TreeItem();
-		TreeItem root=new TreeItem("Attendees");
-		Label [] attendees= new Label[5];
-		
-	      class MyHandler implements ClickHandler {
-	    		
-				
-				public void onClick(ClickEvent event) {
-					
-					greetingService.getAttendeeInfo(((Label) event.getSource()).getText(), new AsyncCallback<String>(){
-						public void onFailure(Throwable caught) {
-						}
+		showAttendees("ddd");
+		// TreeItem hTree =new TreeItem();
 
-						public void onSuccess(String result) {
-							
-							
-						infoBox.setText(result);	
-						}
-						
-					}
-							
-					);
-					
-				}
-	        }
-		for(int i=0;i<5;i++)
-		{
-			attendees[i]=new Label("attendees"+i);
-			root.addItem(attendees[i]);
-			attendees[i].addClickHandler(new MyHandler());
-			
+	}
+
+	class MyHandler implements ClickHandler {
+
+		public void onClick(ClickEvent event) {
+			infoBox.setText(((MyLabel) event.getSource()).getAttendeeInfo());
+			/*
+			 * greetingService.getAttendeeInfo(((Label)
+			 * event.getSource()).getText(), new AsyncCallback<String>(){ public
+			 * void onFailure(Throwable caught) { }
+			 * 
+			 * public void onSuccess(String result) {
+			 * 
+			 * 
+			 * infoBox.setText(result); }
+			 * 
+			 * }
+			 * 
+			 * );
+			 */
+		}
+	}
+
+	class MyLabel extends Label {
+		public MyLabel(String m) {
+			super(m);
 		}
 		
-		Tree hTree=new Tree();
-		hTree.addItem(root);
+		String attendeeInfo = null;
+
+		void setAttendeeInfo(String info) {
+			attendeeInfo = info;
+		}
+
+		String getAttendeeInfo() {
+			return attendeeInfo;
+		}
+	}
+
+	public static void showAttendees(String eventID) {
 		
-		hPanel.	add(hTree);
-		hPanel.	add(infoBox);
+			attendeeService.getAttendeeList(eventID, new AsyncCallback<ArrayList<Attendee>>(){
+				public void onFailure(Throwable caught){}
+				public	void onSuccess(ArrayList<Attendee> attendeeList){
+				attendees = new MyLabel[attendeeList.size()];
+
+				for (int i = 0; i < attendeeList.size(); i++) {
+					attendees[i] = new MyLabel(attendeeList.get(i).getName());
+					attendees[i].setAttendeeInfo(attendeeList.get(i).toString());
+					root.addItem(attendees[i]);
+					attendees[i].addClickHandler(new MyHandler());
+				}
+				} 
+			});
 
 
-  
-        
-        
+		Tree hTree = new Tree();
+		hTree.addItem(root);
+
+		hPanel.add(hTree);
+		hPanel.add(infoBox);
 	}
 
 }
-
-
