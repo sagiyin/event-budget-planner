0c2806c2a0dfe73f72deec0972ba565e9bd5c0ae
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-16 02:22:20 +0000

    Remove throws from AttendeeService

--- src/budgeteventplanner/client/AttendeeServiceAsync.java
+++ src/budgeteventplanner/client/AttendeeServiceAsync.java
@@ -1,6 +1,5 @@
 package budgeteventplanner.client;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import budgeteventplanner.client.entity.Attendee;
@@ -8,10 +7,10 @@ import budgeteventplanner.client.entity.Attendee;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface AttendeeServiceAsync {
-	void createAttendee(String firstName, String lastName, String email, AsyncCallback<Void> callback) throws NoSuchAlgorithmException;
-	void getAttendeeList(String eventOrganizerID, AsyncCallback<ArrayList<Attendee>> callback) throws NoSuchAlgorithmException;
+	void createAttendee(String firstName, String lastName, String email, AsyncCallback<Void> callback);
+	void getAttendeeList(String eventOrganizerID, AsyncCallback<ArrayList<Attendee>> callback);
 	void saveAttendeeInfo(String attendeeID,  String email, 
 			String firstName, String midName, String lastName, 
-			String jobTitle, String companyName, String address, String phoneNum, AsyncCallback<Void> callback) throws NoSuchAlgorithmException;
+			String jobTitle, String companyName, String address, String phoneNum, AsyncCallback<Void> callback);
 
 }
