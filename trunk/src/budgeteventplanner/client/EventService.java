c227e61a7425d5dce52b15c5048ec2525d604a29
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-19 23:30:47 +0000

    Event Service Interface and Implementation

--- trunk/src/budgeteventplanner/client/EventService.java
+++ trunk/src/budgeteventplanner/client/EventService.java
@@ -1,12 +1,21 @@
 package budgeteventplanner.client;
 
-import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
+
+import budgeteventplanner.client.entity.Event;
+import budgeteventplanner.client.entity.Service;
+import budgeteventplanner.client.entity.ServiceRequest;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("event")
 public interface EventService extends RemoteService {
-	void createEvent(String eventName, String organizer, Integer visibility) throws NoSuchAlgorithmException;
-
+	void createEvent(String organizerId, String name, Date startTime, Date endTime, String address);
+	void updateEventByEventId(String eventId, String name, Date startTime, Date endTime, String address, Integer status);
+	List<Event> getEventsByOrganizerId(String organizerId);
+	void addServiceRequest(String serviceId, String eventId, String name, Date dueDate);
+	List<ServiceRequest> getServiceRequestsByEventId(String eventId);
+	List<Service> getServicesByCategoryId(String categoryId);
 }
--- trunk/src/budgeteventplanner/server/EventServiceImpl.java
+++ trunk/src/budgeteventplanner/server/EventServiceImpl.java
@@ -1,13 +1,17 @@
 package budgeteventplanner.server;
 
-import java.security.NoSuchAlgorithmException;
+import java.util.Date;
+import java.util.List;
 
 import budgeteventplanner.client.EventService;
 import budgeteventplanner.client.entity.Event;
+import budgeteventplanner.client.entity.Service;
+import budgeteventplanner.client.entity.ServiceRequest;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
+import com.googlecode.objectify.Query;
 
 @SuppressWarnings("serial")
 public class EventServiceImpl extends RemoteServiceServlet implements EventService 
@@ -18,13 +22,48 @@ public class EventServiceImpl extends RemoteServiceServlet implements EventServi
 	}
 
 	@Override
-	public void createEvent(String eventName, String organizer, Integer visibility)
-			throws NoSuchAlgorithmException {
-		// TODO Auto-generated method stub
+	public void createEvent(String organizerId, String name, Date startTime,
+			Date endTime, String address) {
 		Objectify ofy = ObjectifyService.begin();
-		Event event= new Event.Builder(eventName, organizer, visibility)
-		.build();
+		Event event= new Event.Builder(organizerId, name, startTime, endTime, address).build();
 		ofy.put(event);
-		
+	}
+
+	@Override
+	public void updateEventByEventId(String eventId, String name,
+			Date startTime, Date endTime, String address, Integer status) {
+		Objectify ofy = ObjectifyService.begin();
+		Event oldEvent = ofy.query(Event.class).filter("eventId", eventId).get();
+		Event newEvent = new Event.Builder(oldEvent).setName(name).setStartTime(startTime).setEndTime(endTime).setAddress(address).setStatus(status).build();
+		ofy.put(newEvent);
+	}
+
+	@Override
+	public List<Event> getEventsByOrganizerId(String organizerId) {
+		Objectify ofy = ObjectifyService.begin();
+		Query<Event> q = ofy.query(Event.class).filter("organizerId", organizerId);
+		return q.list();
+	}
+
+	@Override
+	public void addServiceRequest(String serviceId, String eventId,
+			String name, Date dueDate) {
+		Objectify ofy = ObjectifyService.begin();
+		ServiceRequest request = new ServiceRequest.Builder(serviceId, eventId, name, dueDate).build();
+		ofy.put(request);
+	}
+
+	@Override
+	public List<ServiceRequest> getServiceRequestsByEventId(String eventId) {
+		Objectify ofy = ObjectifyService.begin();
+		Query<ServiceRequest> q = ofy.query(ServiceRequest.class).filter("eventId", eventId);
+		return q.list();		
+	}
+
+	@Override
+	public List<Service> getServicesByCategoryId(String categoryId) {
+		Objectify ofy = ObjectifyService.begin();
+		Query<Service> q = ofy.query(Service.class).filter("categoryId", categoryId);
+		return q.list();
 	}
 }
