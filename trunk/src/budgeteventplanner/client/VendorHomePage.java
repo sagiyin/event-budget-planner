27e44d9961343849e3f5ffd8934817a4427932fa
michaelrobtemp@gmail.com <michaelrobtemp@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-19 21:13:10 +0000

    UI OK

--- trunk/src/budgeteventplanner/client/VendorHomePage.java
+++ trunk/src/budgeteventplanner/client/VendorHomePage.java
@@ -52,8 +52,8 @@ public class VendorHomePage implements EntryPoint {
 	HorizontalPanel addPanel;
 	ListBox category;
 	TextBox service;
-	//TextBox/TextArea description;
-	//TO BE DECIDED
+	TextArea description;
+	TextBox price;
 	Button addService;
 	
 	Hyperlink acceptHyperlink[];
@@ -105,37 +105,51 @@ public class VendorHomePage implements EntryPoint {
 		initializeCategory();
 		 
 		service = new TextBox();
-		//TextArea description = new TextArea();
-	    //description.setCharacterWidth(80);
-	    //description.setVisibleLines(50);
-		addService = new Button("ADD");
-		 /* 
+		description = new TextArea();
+		price = new TextBox();
+	    //description.setCharacterWidth(80); optional size method
+		description.setWidth("600px");
+	    description.setVisibleLines(1);
+		addService = new Button("ADD"); 
 		addService.addClickHandler(new ClickHandler()
 		{
 			public void onClick(ClickEvent event)
 			{
 				String sentCategory = category.getItemText(category.getSelectedIndex());
 				String sentText = service.getText();
-				//TODO description
+				String sentDescription = description.getText();
+				double sentPrice = Double.parseDouble(price.getText());
 				
 				//add to server
 				
 				//refresh the table
+				
+				existingService.setWidget(6, 0, new Label(sentCategory));
+				deleteHyperlink[5] = new Hyperlink("delete", Integer.toString(5));
+				existingService.setWidget(6, 1, new Label(sentText));
+				existingService.setWidget(6, 2, new Label(sentDescription));
+				existingService.setWidget(6, 3, new Label(price.getText()));
+				existingService.setWidget(6, 4, deleteHyperlink[5]);
 			}
-		});*/
+		});
 		
 		
 		addPanel.add(category);
 		addPanel.add(service);
-		//addPanel.add(description);
+		addPanel.add(description);
+		addPanel.add(price);
 		addPanel.add(addService);
-		//vendorService.add(addService, DockPanel.NORTH);
 		vendorService.add(existingService, DockPanel.CENTER);
 		vendorService.add(addPanel, DockPanel.SOUTH);
-		addPanel.setSize("100%", "100%");
-		category.setSize("30%", "100%");
-		service.setSize("40%", "100%");
-		//addService.setSize("20%", "100%");
+		category.setSize("150px", "100%");
+		addPanel.setCellWidth(category, "200px");
+		service.setSize("300px", "100%");
+		addPanel.setCellWidth(service, "350px");
+		addPanel.setCellWidth(description, "650px");
+		price.setWidth("100px");
+		addPanel.setCellWidth(price, "150px");
+		addService.setWidth("100px");
+		addPanel.setCellWidth(addService, "150px");
 		
 		tab.add(vendorPage, "Vendor");
 		tab.add(vendorService, "Service");
@@ -149,10 +163,6 @@ public class VendorHomePage implements EntryPoint {
 		tab.setSize("100%", "100%");
 		//tab initialization
 		
-		//acceptButton = new Button("Accept");
-		//ignoreButton = new Button("Ignore");
-		//viewButton = new Button("View");
-		//buttons initialization
 		
 	}
 	
@@ -160,12 +170,63 @@ public class VendorHomePage implements EntryPoint {
 	{
 		//get data from the service, list the existing services
 		existingService.getColumnFormatter().setWidth(0, "200px");
-		existingService.getColumnFormatter().setWidth(1, "1100px");
-		existingService.getColumnFormatter().setWidth(2, "150px");
+		existingService.getColumnFormatter().setWidth(1, "350px");
+		existingService.getColumnFormatter().setWidth(2, "650px");
+		existingService.getColumnFormatter().setWidth(3, "150px");
+		existingService.getColumnFormatter().setWidth(4, "150px");
 		existingService.setWidget(0, 0, new Label("Category"));
 		existingService.setWidget(0, 1, new Label("Service"));
-		//existingService.setWidget(0, 0, new Label("Description"));
-		existingService.setWidget(0, 2, new Label("Option"));
+		existingService.setWidget(0, 2, new Label("Description"));
+		existingService.setWidget(0, 3, new Label("Price"));
+		existingService.setWidget(0, 4, new Label("Option"));
+		
+		refreshExistingService();
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void refreshExistingService()
+	{
+		existingService.clear();
+		
+		existingService.getColumnFormatter().setWidth(0, "200px");
+		existingService.getColumnFormatter().setWidth(1, "350px");
+		existingService.getColumnFormatter().setWidth(2, "650px");
+		existingService.getColumnFormatter().setWidth(3, "150px");
+		existingService.getColumnFormatter().setWidth(4, "150px");
+		existingService.setWidget(0, 0, new Label("Category"));
+		existingService.setWidget(0, 1, new Label("Service"));
+		existingService.setWidget(0, 2, new Label("Description"));
+		existingService.setWidget(0, 3, new Label("Price"));
+		existingService.setWidget(0, 4, new Label("Option"));
+		//List<Category> getExistingCategory(String VendorID);
+		
+		
+		Date today = new Date();
+		final Label[] a = new Label[11]; //TODO
+		for(int i = 0; i < 11; i++)
+			a[i] =  new Label(today.toString());
+		
+		
+		deleteHyperlink = new Hyperlink[6];
+		for(int i = 0; i < 5; i++)
+		{
+			existingService.setWidget(i+1, 0, new Label(Integer.toString(i)));
+			deleteHyperlink[i] = new Hyperlink("delete", Integer.toString(i));
+			existingService.setWidget(i+1, 1, new Label("lol idk the service name"));
+			existingService.setWidget(i+1, 2, a[i]);
+			existingService.setWidget(i+1, 3, new Label("10"));
+			existingService.setWidget(i+1, 4, deleteHyperlink[i]);
+			
+			//listener add
+			deleteHyperlink[i].addClickHandler(new ClickHandler()
+			{
+				public void onClick(ClickEvent event)
+				{
+					//void deleteExistingService(String ServiceID, String VendorID);
+					refreshExistingService();
+				}
+			});
+		}
 	}
 	
 	public void initializeCategory()
