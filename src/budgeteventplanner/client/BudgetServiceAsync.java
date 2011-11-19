0fe66859cd735ea000ce463ec1a3e7d4444f7a64
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-19 21:50:46 +0000

    Fix legacy exceptions

--- src/budgeteventplanner/client/BudgetServiceAsync.java
+++ src/budgeteventplanner/client/BudgetServiceAsync.java
@@ -1,10 +1,7 @@
 package budgeteventplanner.client;
 
-import java.security.NoSuchAlgorithmException;
-
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 public interface BudgetServiceAsync {
-	void createBudget(String budgetName, AsyncCallback<Void> callback) throws NoSuchAlgorithmException;
-	
+	void createBudget(String budgetName, AsyncCallback<Void> callback);
 }
--- src/budgeteventplanner/server/BudgetServiceImpl.java
+++ src/budgeteventplanner/server/BudgetServiceImpl.java
@@ -1,7 +1,5 @@
 package budgeteventplanner.server;
 
-import java.security.NoSuchAlgorithmException;
-
 import budgeteventplanner.client.BudgetService;
 import budgeteventplanner.client.entity.Attendee;
 import budgeteventplanner.client.entity.Budget;
@@ -11,21 +9,16 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 
 @SuppressWarnings("serial")
-public class BudgetServiceImpl extends RemoteServiceServlet implements BudgetService 
-{
-	public BudgetServiceImpl() 
-	{
+public class BudgetServiceImpl extends RemoteServiceServlet implements
+		BudgetService {
+	public BudgetServiceImpl() {
 		ObjectifyService.register(Attendee.class);
 	}
-	
+
 	@Override
-	public void createBudget(String budegetName)
-			throws NoSuchAlgorithmException {
-		// TODO Auto-generated method stub
+	public void createBudget(String budegetName) {
 		Objectify ofy = ObjectifyService.begin();
-		Budget attendee= new Budget.Builder(budegetName)
-		.build();
+		Budget attendee = new Budget.Builder(budegetName).build();
 		ofy.put(attendee);
-		
 	}
 }
