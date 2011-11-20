2aa88f7726d96a26cb2578c78e8e6fbc56198337
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-20 00:06:24 +0000

    VendorService interface and implementation

--- src/budgeteventplanner/client/VendorService.java
+++ src/budgeteventplanner/client/VendorService.java
@@ -1,9 +1,7 @@
 package budgeteventplanner.client;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-import budgeteventplanner.client.entity.Category;
 import budgeteventplanner.client.entity.Service;
 import budgeteventplanner.client.entity.ServiceRequest;
 
@@ -12,34 +10,9 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("vendor")
 public interface VendorService extends RemoteService {
-	
-	//add a new sevice to a vendor’s service list
 	void addService(String categoryId, String vendorId, String name, Double price, String description);
-
-	//get the vendor’s existing service list
-	List<Category> getExistingCategory(String VendorID);
-
-	//delete a service from the vendor’s service list
-	void deleteExistingService(String ServiceID, String VendorID);
-
-	//get all categories from the server
-	List<Category> getAllCategory();
-
-	//get all pending events from server
-	//TODO new entity
-	List<ServiceRequest> getPendingServiceRequest(String VendorID);
-
-	//get all accepted events from server
-	List<ServiceRequest> getAcceptedServiceRequest(String VendorID);
-
-	//get all ignored events from server
-	List<ServiceRequest> getIgnoredserviceRequest(String VendorID);
-
-	//set a event from pending to ignore
-	void ignoreServiceRequest(String VendorID, String ServiceRequestID);
-
-	//set a event from pending to accepted
-	void acceptServiceRequest(String VendorID, String ServiceRequestID);
-	
-	
+	void deleteService(String serviceId);
+	List<Service> getServiceByVendorId(String vendorId);
+	List<ServiceRequest> getServiceRequestByStatus(String vendorId, String status);
+	void updateServiceRequestStatus(String serviceRequestId, Integer status);
 }
--- src/budgeteventplanner/server/VendorServiceImpl.java
+++ src/budgeteventplanner/server/VendorServiceImpl.java
@@ -1,13 +1,11 @@
 package budgeteventplanner.server;
 import java.util.ArrayList;
 import java.util.List;
+
 import budgeteventplanner.client.VendorService;
-import budgeteventplanner.client.entity.Category;
 import budgeteventplanner.client.entity.Service;
 import budgeteventplanner.client.entity.ServiceRequest;
-import budgeteventplanner.client.entity.User;
 
-import com.google.appengine.repackaged.com.google.common.collect.Lists;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
@@ -16,158 +14,55 @@ import com.googlecode.objectify.Query;
 @SuppressWarnings("serial")
 public class VendorServiceImpl extends RemoteServiceServlet implements VendorService 
 {
-	public VendorServiceImpl() 
-	{
-		ObjectifyService.register(User.class);
-	}
-		
+	public VendorServiceImpl() {}
+
 	@Override
-	public void addService(String categoryId, String vendorId, String name, Double price, String description)
-	{
+	public void addService(String categoryId, String vendorId, String name,
+			Double price, String description) {
 		Objectify ofy = ObjectifyService.begin();
-		Service srv = new Service.Builder(categoryId, vendorId, name, price, description).build();
-		ofy.put(srv);
+		Service service= new Service.Builder(categoryId, vendorId, name, price, description).build();
+		ofy.put(service);
 	}
-	
+
 	@Override
-	public List<Category> getExistingCategory(String VendorID)
-	{
-		List<Service> vendorSvcList = getServiceByID(VendorID);
-		List<Category> catList = new ArrayList<Category>();
-		for(Service svc : vendorSvcList)
-		{
-			catList.add(getCategoryByID(svc.getCategoryId()));
+	public void deleteService(String serviceId) {
+		Objectify ofy = ObjectifyService.begin();
+		ofy.delete(serviceId);
+	}
+
+	@Override
+	public List<Service> getServiceByVendorId(String vendorId) {
+		Objectify ofy = ObjectifyService.begin();
+		Query<Service> q = ofy.query(Service.class).filter("vendorId", vendorId);
+		return q.list();
+	}
+
+	@Override
+	public List<ServiceRequest> getServiceRequestByStatus(String vendorId,
+			String status) {
+		Objectify ofy = ObjectifyService.begin();
+		List<ServiceRequest> result = new ArrayList<ServiceRequest>();
+		
+		Query<Service> queryService = ofy.query(Service.class).filter("vendorId", vendorId);
+		for (Service s : queryService) {
+			Query<ServiceRequest> queryServiceRequest = ofy.query(ServiceRequest.class).filter("serviceId", s.getServiceId());
+			for (ServiceRequest request : queryServiceRequest) {
+				if (request.getStatus().equals(status)) {
+					result.add(request);
+				}
+			}
 		}
-		
-		List<String> catIdList = new ArrayList<String>();
-		List<Category> newCatList = new ArrayList<Category>();
-		for(Category c : catList) 
-		{
-			if (!catIdList.contains(c.getCategoryId())) 
-			{
-				catIdList.add(c.getCategoryId());
-				newCatList.add(c);
-			}
-		}
-		
-		return newCatList;
-	}
-	
-	@Override
-	public void deleteExistingService(String ServiceID, String VendorID)
-	{
-		Objectify ofy = ObjectifyService.begin();
-	//	Service svc= ofy.query(Service.class).filter("id", ServiceID).get();
-		
-		ofy.delete(Service.class, ServiceID);
-	}
-	
-	@Override
-	public List<Category> getAllCategory()
-	{
-		Objectify ofy = ObjectifyService.begin();
-		@SuppressWarnings("unchecked")
-		List<Category> list = (List<Category>)(ofy.query(Category.class).fetch());
-	//	ArrayList<Category> result = Lists.newArrayList();
-		
-		return list;
+		return result;
 	}
 
 	@Override
-	public List<ServiceRequest> getPendingServiceRequest(String VendorID) 
-	{
+	public void updateServiceRequestStatus(String serviceRequestId,
+			Integer status) {
 		Objectify ofy = ObjectifyService.begin();
-		List<ServiceRequest> pendingSvc = new ArrayList<ServiceRequest>();
+		ServiceRequest oldRequest = ofy.query(ServiceRequest.class).filter("serviceRequestId", serviceRequestId).get();
+		ServiceRequest newRequest = new ServiceRequest.Builder(oldRequest).setStatus(status).build();
+		ofy.put(newRequest);
+	}
 		
-		Query<Service> svc = ofy.query(Service.class).filter("vendorId", VendorID);
-		
-		for (Service s : svc) 
-		{
-			Query<ServiceRequest> svcReq = ofy.query(ServiceRequest.class).filter("serviceId", s.getServiceId());
-			for (ServiceRequest request : svcReq)
-			{
-				if (request.getStatus() == ServiceRequest.PENDING) 
-				{
-					pendingSvc.add(request);
-				}
-			}
-		}			
-		return pendingSvc;
 
-	}
-
-	@Override
-	public List<ServiceRequest> getAcceptedServiceRequest(String VendorID) {
-		Objectify ofy = ObjectifyService.begin();
-		List<ServiceRequest> acceptedSvc = new ArrayList<ServiceRequest>();
-		
-		Query<Service> svc = ofy.query(Service.class).filter("vendorId", VendorID);
-		
-		for (Service s : svc) 
-		{
-			Query<ServiceRequest> svcReq = ofy.query(ServiceRequest.class).filter("serviceId", s.getServiceId());
-			for (ServiceRequest request : svcReq)
-			{
-				if (request.getStatus() == ServiceRequest.ACCEPTED) 
-				{
-					acceptedSvc.add(request);
-				}
-			}
-		}			
-		return acceptedSvc;
-	}
-
-	@Override
-	public List<ServiceRequest> getIgnoredserviceRequest(String VendorID) 
-	{
-		Objectify ofy = ObjectifyService.begin();
-		List<ServiceRequest> ignoredSvc = new ArrayList<ServiceRequest>();
-		
-		Query<Service> svc = ofy.query(Service.class).filter("vendorId", VendorID);
-		
-		for (Service s : svc) 
-		{
-			Query<ServiceRequest> svcReq = ofy.query(ServiceRequest.class).filter("serviceId", s.getServiceId());
-			for (ServiceRequest request : svcReq)
-			{
-				if (request.getStatus() == ServiceRequest.IGNORED) 
-				{
-					ignoredSvc.add(request);
-				}
-			}
-		}			
-		return ignoredSvc;
-	}
-
-	@Override
-	public void ignoreServiceRequest(String VendorID, String ServiceRequestID) 
-	{
-		Objectify ofy = ObjectifyService.begin();
-		ServiceRequest svcReq= ofy.query(ServiceRequest.class).filter("id", ServiceRequestID).get();
-		new ServiceRequest.Builder(svcReq).setStatus(ServiceRequest.IGNORED).build(); // Set status to ignored
-		
-		
-	}
-
-	@Override
-	public void acceptServiceRequest(String VendorID, String ServiceRequestID) {
-		Objectify ofy = ObjectifyService.begin();
-		ServiceRequest svcReq= ofy.query(ServiceRequest.class).filter("id", ServiceRequestID).get();
-		new ServiceRequest.Builder(svcReq).setStatus(ServiceRequest.ACCEPTED).build(); // Set status to ignored
-		
-	}
-	
-	// Local Helper Function
-	private List<Service> getServiceByID(String VendorID) 
-	{
-		Objectify ofy = ObjectifyService.begin();		
-		Query<Service> svc = ofy.query(Service.class).filter("vendorId", VendorID);
-		return svc.list();
-	}
-	private Category getCategoryByID(String ServiceID) 
-	{
-		Objectify ofy = ObjectifyService.begin();		
-		Category cat = ofy.query(Category.class).filter("ServiceId", ServiceID).get();
-		return cat;
-	}
 }
