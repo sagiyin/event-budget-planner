0b36614c221a42e9885863c2a7a6f528898bdb7b
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-20 01:35:15 +0000

    update updatecall

--- src/budgeteventplanner/client/AttendeeRegistration.java
+++ src/budgeteventplanner/client/AttendeeRegistration.java
@@ -24,39 +24,41 @@ public class AttendeeRegistration implements EntryPoint {
 			+ "connection and try again.";
 
 	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
+	 * Create a remote service proxy to talk to the server-side Greeting
+	 * service.
 	 */
 	private final AttendeeServiceAsync attendeeService = GWT
 			.create(AttendeeService.class);
-	final Label emailLabel=new Label("*Email:");
-	final TextBox emailBox=new TextBox();
-	final Label firstNameLabel=new Label("*First Name:");
-	final TextBox firstNameBox=new TextBox();
-	final Label midNameLabel=new Label("Mid Name:");
-	final TextBox midNameBox=new TextBox();
-	final Label lastNameLabel=new Label("*Last Name:");
-	final TextBox lastNameBox=new TextBox();
-	final Label jobTitleLabel=new Label("Job Title:");
-	final TextBox jobTitleBox=new TextBox();
-	final Label companyLabel=new Label("Company:");
-	final TextBox companyBox=new TextBox();
-	final Label address1Label=new Label("*Address 1:");
-	final TextBox address1Box=new TextBox();
-	final Label address2Label=new Label("Address 2:");
-	final TextBox address2Box=new TextBox();
-	final Label cityLabel=new Label("*City:");
-	final TextBox cityBox=new TextBox();
-	final Label stateLabel=new Label("*State:");
-	final TextBox stateBox=new TextBox();
-	final Label zipLabel=new Label("*Zip:");
-	final TextBox zipBox=new TextBox();
-	final Label phoneLabel=new Label("*Phone Number:");
-	final TextBox phoneBox=new TextBox();
-	final Button submit=new Button("submit");
-	final DialogBox dialogBox=new DialogBox();
+	final Label emailLabel = new Label("*Email:");
+	final TextBox emailBox = new TextBox();
+	final Label firstNameLabel = new Label("*First Name:");
+	final TextBox firstNameBox = new TextBox();
+	final Label midNameLabel = new Label("Mid Name:");
+	final TextBox midNameBox = new TextBox();
+	final Label lastNameLabel = new Label("*Last Name:");
+	final TextBox lastNameBox = new TextBox();
+	final Label jobTitleLabel = new Label("Job Title:");
+	final TextBox jobTitleBox = new TextBox();
+	final Label companyLabel = new Label("Company:");
+	final TextBox companyBox = new TextBox();
+	final Label address1Label = new Label("*Address 1:");
+	final TextBox address1Box = new TextBox();
+	final Label address2Label = new Label("Address 2:");
+	final TextBox address2Box = new TextBox();
+	final Label cityLabel = new Label("*City:");
+	final TextBox cityBox = new TextBox();
+	final Label stateLabel = new Label("*State:");
+	final TextBox stateBox = new TextBox();
+	final Label zipLabel = new Label("*Zip:");
+	final TextBox zipBox = new TextBox();
+	final Label phoneLabel = new Label("*Phone Number:");
+	final TextBox phoneBox = new TextBox();
+	final Button submit = new Button("submit");
+	final DialogBox dialogBox = new DialogBox();
 	final Button closeButton = new Button("Close");
 	VerticalPanel dialogVPanel = new VerticalPanel();
-	private static String attendeeID=null;
+	private static String attendeeID = null;
+
 	/**
 	 * This is the entry point method.
 	 */
@@ -64,63 +66,49 @@ public class AttendeeRegistration implements EntryPoint {
 	public void onModuleLoad() {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
-		
+
 		RootPanel.get("email").add(emailLabel);
 		RootPanel.get("emailBox").add(emailBox);
-		
-		
+
 		RootPanel.get("firstName").add(firstNameLabel);
 		RootPanel.get("firstNameBox").add(firstNameBox);
-	
 
 		RootPanel.get("midName").add(midNameLabel);
 		RootPanel.get("midNameBox").add(midNameBox);
-		
 
 		RootPanel.get("lastName").add(lastNameLabel);
 		RootPanel.get("lastNameBox").add(lastNameBox);
-		
-		
 
 		RootPanel.get("jobTitle").add(jobTitleLabel);
 		RootPanel.get("jobTitleBox").add(jobTitleBox);
-		
 
 		RootPanel.get("company").add(companyLabel);
 		RootPanel.get("companyBox").add(companyBox);
-		
 
 		RootPanel.get("address1").add(address1Label);
 		RootPanel.get("address1Box").add(address1Box);
-		
 
 		RootPanel.get("address2").add(address2Label);
 		RootPanel.get("address2Box").add(address2Box);
-		
 
 		RootPanel.get("city").add(cityLabel);
 		RootPanel.get("cityBox").add(cityBox);
-		
 
 		RootPanel.get("state").add(stateLabel);
 		RootPanel.get("stateBox").add(stateBox);
-		
 
 		RootPanel.get("zip").add(zipLabel);
 		RootPanel.get("zipBox").add(zipBox);
-		
 
 		RootPanel.get("phone").add(phoneLabel);
 		RootPanel.get("phoneBox").add(phoneBox);
-		
-	
-		RootPanel.get("submit").add(submit);
-		//dialog box
 
-		
+		RootPanel.get("submit").add(submit);
+		// dialog box
+
 		dialogVPanel.addStyleName("dialogVPanel");
 		dialogVPanel.add(new HTML("<b>Submit Successful!</b>"));
-	
+
 		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
 		dialogVPanel.add(closeButton);
 		dialogBox.setWidget(dialogVPanel);
@@ -132,106 +120,126 @@ public class AttendeeRegistration implements EntryPoint {
 				submit.setEnabled(true);
 				submit.setFocus(true);
 			}
-		});   
-		//emailBox.setFocus(true);
-		
-	
-	        class MyHandler implements ClickHandler {
-		
-			
-				public void onClick(ClickEvent event) {
-					
-					if(!checkForm())	return;
-					
-					attendeeService.updateAttendeeInfo(attendeeID, firstNameBox.getName()+" "+midNameBox.getName(),emailBox.getName(),jobTitleBox.getName(), companyBox.getName(), address1Box.getName()+"\n"+ address2Box.getName()+"\n"+cityBox.getName()+"\n"+stateBox.getName()+"\n"+zipBox.getName(),phoneBox.getName(), new AsyncCallback<Void>(){
-						public void onFailure(Throwable caught) {
+		});
+		// emailBox.setFocus(true);
+
+		class MyHandler implements ClickHandler {
+
+			public void onClick(ClickEvent event) {
+
+				if (!checkForm())
+					return;
+
+				attendeeService.updateAttendeeInfo(
+						attendeeID,
+						firstNameBox.getText() + " " + midNameBox.getText(),
+						emailBox.getText(),
+						jobTitleBox.getText(),
+						companyBox.getText(),
+						address1Box.getText() + "\n" + address2Box.getText()
+								+ "\n" + cityBox.getText() + "\n"
+								+ stateBox.getText() + "\n" + zipBox.getText(),
+						phoneBox.getText(), new AsyncCallback<Void>() {
+							public void onFailure(Throwable caught) {
+							}
+
+							public void onSuccess(Void result) {
+
+								((HTML) dialogVPanel.getWidget(0))
+										.setHTML("<b>Submit Successful!</b>");
+								dialogBox.center();
+								closeButton.setFocus(true);
+								// RootPanel.get("attendeeManager").setVisible(true);
+								emailBox.setText("");
+								firstNameBox.setText("");
+								midNameBox.setText("");
+								jobTitleBox.setText("");
+								lastNameBox.setText("");
+								companyBox.setText("");
+								address1Box.setText("");
+								address2Box.setText("");
+								cityBox.setText("");
+								stateBox.setText("");
+								zipBox.setText("");
+								phoneBox.setText("");
+							}
+
 						}
 
-						public void onSuccess(Void result) {
-							
-							((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Submit Successful!</b>");
-							dialogBox.center();
-							closeButton.setFocus(true);
-							//RootPanel.get("attendeeManager").setVisible(true);
-							emailBox.setText("");
-							firstNameBox.setText("");
-							midNameBox.setText("");
-							jobTitleBox.setText("");
-							lastNameBox.setText("");
-							companyBox.setText("");
-							address1Box.setText("");
-							address2Box.setText("");
-							cityBox.setText("");
-							stateBox.setText("");
-							zipBox.setText("");
-							phoneBox.setText("");
-						}
-						
-					}
-							
-					);
-					
-				
-				}
-	        }
-	        MyHandler handler = new MyHandler();
-	        submit.addClickHandler(handler);
+				);
+
+			}
+		}
+		MyHandler handler = new MyHandler();
+		submit.addClickHandler(handler);
 	}
-	
-	public static void setAttendeeID(String attendeeIDc){
-		attendeeID=attendeeIDc;
-	} 
-	boolean checkForm(){
-		if(emailBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your email address</b>");
+
+	// used by BudgetEventPlanner UI to set the current attendeeID
+	public static void setAttendeeID(String attendeeIDc) {
+		attendeeID = attendeeIDc;
+	}
+
+	// check if the form is legal
+	boolean checkForm() {
+		if (emailBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input your email address</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(!(emailBox.getText().contains("@") &&emailBox.getText().contains("."))){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input a legal email address</b>");
+		if (!(emailBox.getText().contains("@") && emailBox.getText().contains(
+				"."))) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input a legal email address</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(firstNameBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your first name</b>");
+		if (firstNameBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input your first name</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(address1Box.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your address</b>");
+		if (address1Box.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input your address</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(cityBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the city you live in</b>");
+		if (cityBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input the city you live in</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(stateBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the state you live in</b>");
+		if (stateBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input the state you live in</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(zipBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input the ZIP code</b>");
+		if (zipBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input the ZIP code</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		if(phoneBox.getText().compareTo("")==0 ){
-			((HTML) dialogVPanel.getWidget(0)).setHTML("<b>Be sure input your phone number</b>");
+		if (phoneBox.getText().compareTo("") == 0) {
+			((HTML) dialogVPanel.getWidget(0))
+					.setHTML("<b>Be sure input your phone number</b>");
 			dialogBox.center();
 			closeButton.setFocus(true);
 			return false;
 		}
-		
-		 return true;
+
+		return true;
 	}
 
 }
