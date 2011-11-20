6d5318b2e7d6116e4a58e819a2821026690ca08e
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-20 00:06:24 +0000

    VendorService interface and implementation

--- trunk/src/budgeteventplanner/client/VendorServiceAsync.java
+++ trunk/src/budgeteventplanner/client/VendorServiceAsync.java
@@ -1,41 +1,17 @@
 package budgeteventplanner.client;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
-
-import budgeteventplanner.client.entity.Category;
-import budgeteventplanner.client.entity.Service;
-import budgeteventplanner.client.entity.ServiceRequest;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import budgeteventplanner.client.entity.Service;
+import budgeteventplanner.client.entity.ServiceRequest;
+
+
 public interface VendorServiceAsync {
-	
-	//add a new sevice to a vendor’s service list
 	void addService(String categoryId, String vendorId, String name, Double price, String description, AsyncCallback<Void> callback);
-
-	//get the vendor’s existing service list
-	void getExistingCategory(String VendorID, AsyncCallback<List<Category>> callback);
-
-	//delete a service from the vendor’s service list
-	void deleteExistingService(String ServiceID, String VendorID, AsyncCallback<Void> callback);
-
-	//get all categories from the server
-	void getAllCategory(AsyncCallback<List<Category>> callback);
-
-	//get all pending events from server
-	//TODO new entity
-	void getPendingServiceRequest(String VendorID, AsyncCallback<List<ServiceRequest>> callback);
-
-	//get all accepted events from server
-	void getAcceptedServiceRequest(String VendorID, AsyncCallback<List<ServiceRequest>> callback);
-
-	//get all ignored events from server
-	void getIgnoredserviceRequest(String VendorID, AsyncCallback<List<ServiceRequest>> callback);
-
-	//set a event from pending to ignore
-	void ignoreServiceRequest(String VendorID, String ServiceRequestID, AsyncCallback<Void> callback);
-
-	//set a event from pending to accepted
-	void acceptServiceRequest(String VendorID, String ServiceRequestID, AsyncCallback<Void> callback);
+	void deleteService(String serviceId, AsyncCallback<Void> callback);
+	void getServiceByVendorId(String vendorId, AsyncCallback<List<Service>> callback);
+	void getServiceRequestByStatus(String vendorId, String status, AsyncCallback<List<ServiceRequest>> callback);
+	void updateServiceRequestStatus(String serviceRequestId, Integer status, AsyncCallback<Void> callback);
 }
