9687be769e64503d85bff0cdbcc5ce4ae7ae9eef
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-26 21:03:04 +0000

    Attendee Service/Implementation: Return Attendee entity instead of AttendeeeId when createAttendee

--- trunk/src/budgeteventplanner/client/AttendeeService.java
+++ trunk/src/budgeteventplanner/client/AttendeeService.java
@@ -9,7 +9,7 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
 @RemoteServiceRelativePath("attendee")
 public interface AttendeeService extends RemoteService {
-	String createAttendee(String eventId, String name, String email);
+	Attendee createAttendee(String eventId, String name, String email);
 	ArrayList<Attendee> getAttendeeListByOrganizerId(String organizerId);
 	ArrayList<Attendee> getAttendeeListByEventId(String eventId);
 	void updateAttendeeInfo(String attendeeId, String name, String email, String jobTitle, String companyName, String address, String phoneNum, Integer status);
--- trunk/src/budgeteventplanner/client/AttendeeServiceAsync.java
+++ trunk/src/budgeteventplanner/client/AttendeeServiceAsync.java
@@ -7,7 +7,7 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import budgeteventplanner.client.entity.Attendee;
 
 public interface AttendeeServiceAsync {
-	void createAttendee(String eventId, String name, String email, AsyncCallback<String> attendeeId);
+	void createAttendee(String eventId, String name, String email, AsyncCallback<Attendee> attendeeId);
 	void getAttendeeListByOrganizerId(String organizerId, AsyncCallback<ArrayList<Attendee>> attendeeList);
 	void getAttendeeListByEventId(String eventId, AsyncCallback<ArrayList<Attendee>> attendeeList);
 	void updateAttendeeInfo(String attendeeId, String name, String email, String jobTitle, String companyName, String address, String phoneNum, Integer status, AsyncCallback<Void> feedback);
