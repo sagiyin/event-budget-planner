addd603226755b19c91b0b08f4ee092ea4eb8df6
xudipurdue@gmail.com <xudipurdue@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-27 00:38:44 +0000

    I have added charts

--- trunk/src/budgeteventplanner/client/DatabaseTestPage.java
+++ trunk/src/budgeteventplanner/client/DatabaseTestPage.java
@@ -4,30 +4,50 @@ import java.util.ArrayList;
 import java.util.Date;
 
 import budgeteventplanner.client.entity.TestEntity;
+import budgeteventplanner.shared.Pair;
+//import com.google.appengine.repackaged.com.google.common.base.Pair;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.visualization.client.AbstractDataTable;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
+import com.google.gwt.visualization.client.DataTable;
+import com.google.gwt.visualization.client.Selection;
+import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.events.SelectHandler;
+import com.google.gwt.visualization.client.visualizations.corechart.BarChart;
+import com.google.gwt.visualization.client.visualizations.corechart.CoreChart;
+import com.google.gwt.visualization.client.visualizations.corechart.Options;
+import com.google.gwt.visualization.client.visualizations.corechart.PieChart;
+
+
 
 public class DatabaseTestPage implements EntryPoint {
 
 	private final DataServiceAsync dataService = GWT.create(DataService.class);
 
 	//private final UserServiceAsync userService = GWT.create(UserService.class);
-	
+
 	private final EventServiceAsync eventService = GWT.create(EventService.class);
 
+	private final BudgetServiceAsync budgetService = GWT.create(BudgetService.class);
+
 	@Override
-	public void onModuleLoad() {
+	public void onModuleLoad() 
+	{
 		// Create the popup dialog box
 		final DialogBox dialogBox = new DialogBox();
 		dialogBox.setText("Remote Procedure Call");
@@ -53,6 +73,7 @@ public class DatabaseTestPage implements EntryPoint {
 			}
 		});
 
+
 		// database component
 		final Button putButton = new Button("Put");
 		final Button getButton = new Button("Get");
@@ -71,29 +92,29 @@ public class DatabaseTestPage implements EntryPoint {
 			@SuppressWarnings("deprecation")
 			@Override
 			public void onClick(ClickEvent event) {
-				eventService.addServiceRequest("5ADD1AFE-0E73-4923-B9C3-78A3130EFDC7", "XXX", "food for today",1, new Date(2011, 11, 23), new AsyncCallback<Void>() {
-							@Override
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML("RPC Call Failed!");
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
+				eventService.addServiceRequest("5ADD1AFE-0E73-4923-B9C3-78A3130EFDC7", "XXX", "food for today", null, new Date(2011, 11, 23), new AsyncCallback<Void>() {
+					@Override
+					public void onFailure(Throwable caught) {
+						// Show the RPC error message to the user
+						dialogBox
+						.setText("Remote Procedure Call - Failure");
+						serverResponseLabel
+						.addStyleName("serverResponseLabelError");
+						serverResponseLabel.setHTML("RPC Call Failed!");
+						dialogBox.center();
+						closeButton.setFocus(true);
+					}
 
-							@Override
-							public void onSuccess(Void result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML("Success");
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
+					@Override
+					public void onSuccess(Void result) {
+						dialogBox.setText("Remote Procedure Call");
+						serverResponseLabel
+						.removeStyleName("serverResponseLabelError");
+						serverResponseLabel.setHTML("Success");
+						dialogBox.center();
+						closeButton.setFocus(true);
+					}
+				});
 			}
 
 		}
@@ -112,9 +133,9 @@ public class DatabaseTestPage implements EntryPoint {
 							public void onFailure(Throwable caught) {
 								// Show the RPC error message to the user
 								dialogBox
-										.setText("Remote Procedure Call - Failure");
+								.setText("Remote Procedure Call - Failure");
 								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
+								.addStyleName("serverResponseLabelError");
 								serverResponseLabel.setHTML("RPC Call Failed!");
 								dialogBox.center();
 								closeButton.setFocus(true);
@@ -124,7 +145,7 @@ public class DatabaseTestPage implements EntryPoint {
 							public void onSuccess(ArrayList<TestEntity> result) {
 								dialogBox.setText("Remote Procedure Call");
 								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
+								.removeStyleName("serverResponseLabelError");
 								String html = new String();
 								for (TestEntity ent : result) {
 									html += ent.toString();
@@ -138,193 +159,137 @@ public class DatabaseTestPage implements EntryPoint {
 		}
 
 		DBGetHandler dbgethandler = new DBGetHandler();
-		getButton.addClickHandler(dbgethandler);
+		getButton.addClickHandler(dbgethandler);	
 
-		/*
-		
-		//register component
-		final Button registerButton = new Button("Register");
-		final Button loginButton = new Button("Login");
-		final TextBox dbemail = new TextBox();
-		final TextBox dbrole = new TextBox();
-		final TextBox dbpasswd = new TextBox();
 
-		RootPanel.get("registerContainer").add(dbemail);
-		RootPanel.get("registerContainer").add(dbpasswd);
-		RootPanel.get("registerContainer").add(dbrole);
-		RootPanel.get("registerContainer").add(registerButton);
-		RootPanel.get("registerContainer").add(loginButton);
-
-		class DBRegisterHandler implements ClickHandler {
+		final Button getChart= new Button("chart");
+		RootPanel.get("databseContainer").add(getChart);
+		class DBChartHandler implements ClickHandler 
+		{
 
 			@Override
 			public void onClick(ClickEvent event) {
-				try {
-					userService.register(dbemail.getText(), dbpasswd.getText(),
-							Integer.parseInt(dbrole.getText()),
-							new AsyncCallback<Void>() {
-								@Override
-								public void onFailure(Throwable caught) {
-									dialogBox
-											.setText("Remote Procedure Call - Failure");
-									serverResponseLabel
-											.addStyleName("serverResponseLabelError");
-									serverResponseLabel
-											.setHTML("RPC Call Failed!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
+				budgetService.getSubtotalsByEventId("agtzfnB1cmR1ZWJlcHIvCxIFRXZlbnQiJDRCQ0M5MDc3LUM2QzgtNEVFNS1BNTQzLUYzQjk0RkM3MTZBOQw", new AsyncCallback<ArrayList<Pair<String, Double>>>()
+						{
+					@Override
+					public void onFailure(Throwable caught) {
+						// Show the RPC error message to the user
+						dialogBox
+						.setText("Remote Procedure Call - Failure");
+						serverResponseLabel
+						.addStyleName("serverResponseLabelError");
+						serverResponseLabel.setHTML("RPC Call Failed!");
+						dialogBox.center();
+						closeButton.setFocus(true);
+					}
 
-								@Override
-								public void onSuccess(Void result) {
-									dialogBox.setText("Remote Procedure Call");
-									serverResponseLabel
-											.removeStyleName("serverResponseLabelError");
-									serverResponseLabel
-											.setHTML("Registered Successfully!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-							});
-				} catch (NumberFormatException e) {
-					e.printStackTrace();
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
-			}
 
-		}
-		registerButton.addClickHandler(new DBRegisterHandler());
-		
-		class DBLoginHandler implements ClickHandler {
+					@Override
+					public void onSuccess(ArrayList<Pair<String, Double>> result) {
+						dialogBox.setText("Remote Procedure Call");
+						serverResponseLabel
+						.removeStyleName("serverResponseLabelError");
+						serverResponseLabel.setHTML("Success");
+						dialogBox.center();
+						closeButton.setFocus(true);
 
-			@Override
-			public void onClick(ClickEvent event) {
-				try {
-					userService.login(dbemail.getText(), dbpasswd.getText(),
-							new AsyncCallback<Integer>() {
-								@Override
-								public void onFailure(Throwable caught) {
-									// Show the RPC error message to the user
-									dialogBox
-											.setText("Remote Procedure Call - Failure");
-									serverResponseLabel
-											.addStyleName("serverResponseLabelError");
-									serverResponseLabel.setHTML("RPC Call Failed!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
+						/*-----------------------------------------------------------------*/
+						DataTable data = DataTable.create();
+						data.addColumn(ColumnType.STRING, "Category");
+						data.addColumn(ColumnType.NUMBER, "Cost");
 
-								@Override
-								public void onSuccess(Integer result) {
-									dialogBox.setText("Remote Procedure Call - Success");
-									serverResponseLabel
-											.removeStyleName("serverResponseLabelError");
-									serverResponseLabel.setHTML(result.toString());
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-							});
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
+						int ithRow = 1;
+						int ithCol = 1;
+
+						for (Pair<String, Double> pair : result) {
+							data.addRows(1);
+							data.setValue(ithRow, ithCol++, pair.getValue1());
+							data.setValue(ithRow, ithCol++, pair.getValue2());
+
+							ithRow++;
+							ithCol = 1;
+						}
+
+						final AbstractDataTable finalData = data;
+						/*-----------------------------------------------------------------*/
+						// Create a callback to be called when the visualization API
+						// has been loaded.
+						Runnable onLoadCallback = new Runnable() 
+						{
+							public void run() {
+								Panel panel = RootPanel.get("databseContainer");
+
+								// Create a pie chart visualization.
+								PieChart pie = new PieChart(finalData, createOptions());
+								BarChart bar = new BarChart(finalData, createOptions());
+								pie.addSelectHandler(createSelectHandler(pie));
+								panel.add(pie);
+								panel.add(bar);
+							}
+						};
+
+						// Load the visualization api, passing the onLoadCallback to be called
+						// when loading is done.
+						VisualizationUtils.loadVisualizationApi(onLoadCallback, CoreChart.PACKAGE);
+						/*-----------------------------------------------------------------*/
+						
+					}
+						});
 			}
 		}
-
-		loginButton.addClickHandler(new DBLoginHandler());
-		
-		//event component
-		final Label eventTestLabel = new Label("Event service test");
-		final Button putButton2 = new Button("put2");
-		final Button getButton2 = new Button("get2");
-		final TextBox dbeventname = new TextBox();
-		final TextBox dborganizer = new TextBox();
-		final TextBox dbvisibility = new TextBox();
-
-		RootPanel.get("eventContainer").add(eventTestLabel);
-		RootPanel.get("eventContainer").add(dbeventname);
-		RootPanel.get("eventContainer").add(dborganizer);
-		RootPanel.get("eventContainer").add(dbvisibility);
-		RootPanel.get("eventContainer").add(putButton2);
-		RootPanel.get("eventContainer").add(getButton2);
-
-		class DBPut2Handler implements ClickHandler {
-
-			@Override
-			public void onClick(ClickEvent event) {
-				try {
-					eventService.createEvent(dbeventname.getText(), 
-							dborganizer.getText(), Integer.parseInt(dbvisibility.getText()),
-							new AsyncCallback<Void>() {
-								@Override
-								public void onFailure(Throwable caught) {
-									dialogBox
-											.setText("Remote Procedure Call - Failure");
-									serverResponseLabel
-											.addStyleName("serverResponseLabelError");
-									serverResponseLabel
-											.setHTML("RPC Call Failed!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-
-								@Override
-								public void onSuccess(Void result) {
-									dialogBox.setText("Remote Procedure Call");
-									serverResponseLabel
-											.removeStyleName("serverResponseLabelError");
-									serverResponseLabel
-											.setHTML("Registered Successfully!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-							});
-				} catch (NumberFormatException e) {
-					e.printStackTrace();
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
-			}
-
-		}
-		putButton2.addClickHandler(new DBPut2Handler());
-		
-	/*	class DBGet2Handler implements ClickHandler {
-
-
-			@Override
-			public void onClick(ClickEvent event) {
-				try {
-					userService.login(dbemail2.getText(), dbpasswd.getText(),
-							new AsyncCallback<Integer>() {
-								@Override
-								public void onFailure(Throwable caught) {
-									// Show the RPC error message to the user
-									dialogBox
-											.setText("Remote Procedure Call - Failure");
-									serverResponseLabel
-											.addStyleName("serverResponseLabelError");
-									serverResponseLabel.setHTML("RPC Call Failed!");
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-
-								@Override
-								public void onSuccess(Integer result) {
-									dialogBox.setText("Remote Procedure Call");
-									serverResponseLabel
-											.removeStyleName("serverResponseLabelError");
-									serverResponseLabel.setHTML(result.toString());
-									dialogBox.center();
-									closeButton.setFocus(true);
-								}
-							});
-				} catch (NoSuchAlgorithmException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-		getButton2.addClickHandler(new DBLoginHandler());*/
+		DBChartHandler dbChartHandler = new DBChartHandler();
+		getChart.addClickHandler(dbChartHandler);	
 	}
+
+
+	private Options createOptions() {
+		Options options = Options.create();
+		options.setWidth(400);
+		options.setHeight(240);
+		options.setCurveType("cone");
+		options.setTitle("My Daily Activities");
+		return options;
+	}
+	private SelectHandler createSelectHandler(final PieChart chart) {
+		return new SelectHandler() {
+			@Override
+			public void onSelect(SelectEvent event) {
+				String message = "";
+
+				// May be multiple selections.
+				JsArray<Selection> selections = chart.getSelections();
+
+				for (int i = 0; i < selections.length(); i++) {
+					// add a new line for each selection
+					message += i == 0 ? "" : "\n";
+
+					Selection selection = selections.get(i);
+
+					if (selection.isCell()) {
+						// isCell() returns true if a cell has been selected.
+
+						// getRow() returns the row number of the selected cell.
+						int row = selection.getRow();
+						// getColumn() returns the column number of the selected cell.
+						int column = selection.getColumn();
+						message += "cell " + row + ":" + column + " selected";
+					} else if (selection.isRow()) {
+						// isRow() returns true if an entire row has been selected.
+
+						// getRow() returns the row number of the selected row.
+						int row = selection.getRow();
+						message += "row " + row + " selected";
+					} else {
+						// unreachable
+						message += "Pie chart selections should be either row selections or cell selections.";
+						message += "  Other visualizations support column selections as well.";
+					}
+				}
+
+				Window.alert(message);
+			}
+		};
+	}
+
 
 }
