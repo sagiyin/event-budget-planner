a1a3517f84a9c191d3fca549f75350b5297ba41f
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-11-30 01:15:06 +0000

    Surround try-catch block in constructor

--- src/budgeteventplanner/server/UserServiceImpl.java
+++ src/budgeteventplanner/server/UserServiceImpl.java
@@ -10,40 +10,46 @@ import com.googlecode.objectify.Objectify;
 import com.googlecode.objectify.ObjectifyService;
 
 @SuppressWarnings("serial")
-public class UserServiceImpl extends RemoteServiceServlet implements UserService {
-  public UserServiceImpl() {
-    ObjectifyService.register(User.class);
-  }
+public class UserServiceImpl extends RemoteServiceServlet implements
+		UserService {
+	public UserServiceImpl() {
+		try {
+			ObjectifyService.register(User.class);
+		} catch (Exception e) {
+		}
+	}
 
-  @Override
-  public void register(String email, String password, Integer role) throws NoSuchAlgorithmException {
-    // String encrypted =
-    // MessageDigest.getInstance("SHA-1").digest(password.getBytes()).toString();
-    String encrypted = password;
-    Objectify ofy = ObjectifyService.begin();
-    User user = new User.Builder(email, encrypted, role).build();
-    ofy.put(user);
-  }
+	@Override
+	public void register(String email, String password, Integer role)
+			throws NoSuchAlgorithmException {
+		// String encrypted =
+		// MessageDigest.getInstance("SHA-1").digest(password.getBytes()).toString();
+		String encrypted = password;
+		Objectify ofy = ObjectifyService.begin();
+		User user = new User.Builder(email, encrypted, role).build();
+		ofy.put(user);
+	}
 
-  @Override
-  public Integer login(String email, String password) throws NoSuchAlgorithmException {
-    // String encrypted =
-    // MessageDigest.getInstance("SHA-1").digest(password.getBytes()).toString();
-    String encrypted = password;
-    Objectify ofy = ObjectifyService.begin();
+	@Override
+	public Integer login(String email, String password)
+			throws NoSuchAlgorithmException {
+		// String encrypted =
+		// MessageDigest.getInstance("SHA-1").digest(password.getBytes()).toString();
+		String encrypted = password;
+		Objectify ofy = ObjectifyService.begin();
 
-    User user = ofy.query(User.class).filter("email", email).get();
+		User user = ofy.query(User.class).filter("email", email).get();
 
-    if (encrypted.equals(user.getPassword())) {
-      return user.getRole();
-    } else {
-      return -1;
-    }
-  }
+		if (encrypted.equals(user.getPassword())) {
+			return user.getRole();
+		} else {
+			return -1;
+		}
+	}
 
-  @Override
-  public Integer attendeeLogin(String registrationCode) {
-    return 1;
-  }
+	@Override
+	public Integer attendeeLogin(String registrationCode) {
+		return 1;
+	}
 
 }
