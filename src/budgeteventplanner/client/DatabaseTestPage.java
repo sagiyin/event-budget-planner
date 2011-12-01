ee89bb76ca05980b6530b1d82a16494e57495cd1
sagi.yin@gmail.com <sagi.yin@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-12-01 00:30:00 +0000

    Google Chart Experiment

--- src/budgeteventplanner/client/DatabaseTestPage.java
+++ src/budgeteventplanner/client/DatabaseTestPage.java
@@ -1,14 +1,10 @@
 package budgeteventplanner.client;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import budgeteventplanner.client.entity.Budget;
-import budgeteventplanner.client.entity.BudgetItem;
-import budgeteventplanner.client.entity.TestEntity;
 import budgeteventplanner.shared.Pair;
 
+import com.google.common.collect.Lists;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.core.client.JsArray;
@@ -22,345 +18,156 @@ import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.visualization.client.AbstractDataTable;
-import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.DataTable;
 import com.google.gwt.visualization.client.Selection;
 import com.google.gwt.visualization.client.VisualizationUtils;
+import com.google.gwt.visualization.client.AbstractDataTable.ColumnType;
 import com.google.gwt.visualization.client.events.SelectHandler;
-import com.google.gwt.visualization.client.visualizations.corechart.BarChart;
-import com.google.gwt.visualization.client.visualizations.corechart.CoreChart;
 import com.google.gwt.visualization.client.visualizations.corechart.Options;
 import com.google.gwt.visualization.client.visualizations.corechart.PieChart;
 
-
-
 public class DatabaseTestPage implements EntryPoint {
 
-	private final DataServiceAsync dataService = GWT.create(DataService.class);
+  private final DataServiceAsync dataService = GWT.create(DataService.class);
 
-	//private final UserServiceAsync userService = GWT.create(UserService.class);
+  // private final UserServiceAsync userService = GWT.create(UserService.class);
 
-	private final EventServiceAsync eventService = GWT.create(EventService.class);
+  private final EventServiceAsync eventService = GWT.create(EventService.class);
 
-	private final BudgetServiceAsync budgetService = GWT.create(BudgetService.class);
+  private final BudgetServiceAsync budgetService = GWT.create(BudgetService.class);
 
-	@Override
-	public void onModuleLoad() 
-	{
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
+  private final List<String> categoryName = Lists.newArrayList();
 
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-			}
-		});
+  private final List<Double> limitList = Lists.newArrayList();
 
+  final Boolean loaded = new Boolean(true);
+  @Override
+  public void onModuleLoad() {
+    // Create the popup dialog box
+    final DialogBox dialogBox = new DialogBox();
+    dialogBox.setText("Remote Procedure Call");
+    dialogBox.setAnimationEnabled(true);
+    final Button closeButton = new Button("Close");
+    // We can set the id of a widget by accessing its Element
+    closeButton.getElement().setId("closeButton");
+    final Label textToServerLabel = new Label();
+    final HTML serverResponseLabel = new HTML();
+    VerticalPanel dialogVPanel = new VerticalPanel();
+    dialogVPanel.addStyleName("dialogVPanel");
+    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+    dialogVPanel.add(textToServerLabel);
+    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+    dialogVPanel.add(serverResponseLabel);
+    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+    dialogVPanel.add(closeButton);
+    dialogBox.setWidget(dialogVPanel);
 
-		// database component
-		final Button putButton = new Button("Put");
-		final Button getButton = new Button("Get");
-		final TextBox dbname = new TextBox();
-		final TextBox dbaddress = new TextBox();
-		final TextBox dblimit = new TextBox();
+    closeButton.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        dialogBox.hide();
+      }
+    });
 
-		RootPanel.get("databseContainer").add(dbname);
-		RootPanel.get("databseContainer").add(dbaddress);
-		RootPanel.get("databseContainer").add(dblimit);
-		RootPanel.get("databseContainer").add(putButton);
-		RootPanel.get("databseContainer").add(getButton);
+    Runnable onLoadCallback = new Runnable() {
+      public void run() {
+        Panel panel = RootPanel.get();
 
-		class DBPutHandler implements ClickHandler {
+        // Create a pie chart visualization.
+        PieChart pie = new PieChart(createTable(), createOptions());
 
-			@SuppressWarnings("deprecation")
-			@Override
-			public void onClick(ClickEvent event) {
-				eventService.addServiceRequest("5ADD1AFE-0E73-4923-B9C3-78A3130EFDC7", "XXX", "food for today", null, new Date(2011, 11, 23), new AsyncCallback<Void>() {
-					@Override
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox
-						.setText("Remote Procedure Call - Failure");
-						serverResponseLabel
-						.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML("RPC Call Failed!");
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
+        pie.addSelectHandler(createSelectHandler(pie));
+        panel.add(pie);
+      }
+    };
 
-					@Override
-					public void onSuccess(Void result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel
-						.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML("Success");
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
+    // Load the visualization api, passing the onLoadCallback to be called
+    // when loading is done.
+    VisualizationUtils.loadVisualizationApi(onLoadCallback, PieChart.PACKAGE);
+  }
 
-		}
+  private Options createOptions() {
+    Options options = Options.create();
+    options.setWidth(400);
+    options.setHeight(240);
+    options.setTitle("My Daily Activities");
+    return options;
+  }
 
-		DBPutHandler dbputhandler = new DBPutHandler();
-		putButton.addClickHandler(dbputhandler);
+  private SelectHandler createSelectHandler(final PieChart chart) {
+    return new SelectHandler() {
+      @Override
+      public void onSelect(SelectEvent event) {
+        String message = "";
 
-		class DBGetHandler implements ClickHandler {
+        // May be multiple selections.
+        JsArray<Selection> selections = chart.getSelections();
 
-			@Override
-			public void onClick(ClickEvent event) {
-				dataService.getEntityByLimit(
-						Integer.parseInt(dblimit.getText()),
-						new AsyncCallback<ArrayList<TestEntity>>() {
-							@Override
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-								.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-								.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML("RPC Call Failed!");
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
+        for (int i = 0; i < selections.length(); i++) {
+          // add a new line for each selection
+          message += i == 0 ? "" : "\n";
 
-							@Override
-							public void onSuccess(ArrayList<TestEntity> result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-								.removeStyleName("serverResponseLabelError");
-								String html = new String();
-								for (TestEntity ent : result) {
-									html += ent.toString();
-								}
-								serverResponseLabel.setHTML(html);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
+          Selection selection = selections.get(i);
 
-		DBGetHandler dbgethandler = new DBGetHandler();
-		getButton.addClickHandler(dbgethandler);	
-		
-		
-		
-		
-		
-		final Button initialize= new Button("initialize");
-		RootPanel.get("databseContainer").add(initialize);
-		
-		final TextBox eventIDbox = new TextBox();
-		RootPanel.get("databseContainer").add(eventIDbox);
-		
-		class DBInitializeHandler implements ClickHandler 
-		{
-			String budgetID = "";
-			@Override
-			public void onClick(ClickEvent event) {
-				// Calling method from server
-				budgetService.createBudget(eventIDbox.getText(), "TEST", new AsyncCallback<Budget>() {
-					@Override
-					public void onFailure(Throwable caught) {
-						dialogBox.setText("Screw you...");
-						dialogBox.center();
-					}
+          if (selection.isCell()) {
+            // isCell() returns true if a cell has been selected.
 
-					@Override
-					public void onSuccess(Budget result) {
-						budgetID = result.getBudgetId();
-					}
-				});
-				
-				budgetService.addBudgetItemToBudget("375EDD19-7B4A-4626-A94F-B1E9972CD930", budgetID, 10.0, new AsyncCallback<BudgetItem>(){
-					@Override
-					public void onFailure(Throwable caught) {
-						dialogBox.setText("Screw you...");
-						dialogBox.center();
-					}
+            // getRow() returns the row number of the selected cell.
+            int row = selection.getRow();
+            // getColumn() returns the column number of the selected cell.
+            int column = selection.getColumn();
+            message += "cell " + row + ":" + column + " selected";
+          } else if (selection.isRow()) {
+            // isRow() returns true if an entire row has been selected.
 
-					@Override
-					public void onSuccess(BudgetItem result) {
-						// TODO Auto-generated method stub	
-					}
-				});
-				
-				budgetService.addBudgetItemToBudget("4D389C34-B3B4-4F43-9349-7A92FDF7AE81", budgetID, 15.0, new AsyncCallback<BudgetItem>(){
-					@Override
-					public void onFailure(Throwable caught) {
-						dialogBox.setText("Screw you...");
-						dialogBox.center();
-					}
+            // getRow() returns the row number of the selected row.
+            int row = selection.getRow();
+            message += "row " + row + " selected";
+          } else {
+            // unreachable
+            message += "Pie chart selections should be either row selections or cell selections.";
+            message += "  Other visualizations support column selections as well.";
+          }
+        }
 
-					@Override
-					public void onSuccess(BudgetItem result) {
-						// TODO Auto-generated method stub	
-					}
-				});
-				budgetService.addBudgetItemToBudget("C0985DC3-20AB-4E92-8456-BA71AC2CC572", budgetID, 15.0, new AsyncCallback<BudgetItem>(){
-					@Override
-					public void onFailure(Throwable caught) {
-						dialogBox.setText("Screw you...");
-						dialogBox.center();
-					}
+        Window.alert(message);
+      }
+    };
+  }
 
-					@Override
-					public void onSuccess(BudgetItem result) {
-						// TODO Auto-generated method stub	
-					}
-				});
-			}
-		}
-		initialize.addClickHandler(new DBInitializeHandler());
+  private AbstractDataTable createTable() {
+    
+    DataTable data = DataTable.create();
+    budgetService.getLimitsByBudgetId("7C1A90FE-A1A2-41A5-8A48-AB7746201829",
+        new AsyncCallback<List<Pair<String, Double>>>() {
+          @Override
+          public void onFailure(Throwable caught) {
+          }
 
-		final Button getChart= new Button("chart");
-		RootPanel.get("databseContainer").add(getChart);
-		class DBChartHandler implements ClickHandler 
-		{
-			@Override
-			public void onClick(ClickEvent event) {
-				budgetService.getSubtotalsByEventId(eventIDbox.getText(), new AsyncCallback<List<Pair<String, Double>>>()
-						
-						{
-					@Override
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox
-						.setText("Remote Procedure Call - Failure");
-						serverResponseLabel
-						.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML("RPC Call Failed!");
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-
-					@Override
-					public void onSuccess(List<Pair<String, Double>> result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel
-						.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML("Success");
-						dialogBox.center();
-						closeButton.setFocus(true);
-						
-						/*-----------------------------------------------------------------*/
-						DataTable data = DataTable.create();
-						data.addColumn(ColumnType.STRING, "Category");
-						data.addColumn(ColumnType.NUMBER, "Cost");
-
-						int ithRow = 1;
-						int ithCol = 1;
-
-						for (Pair<String, Double> pair : result) {
-							data.addRows(1);
-							data.setValue(ithRow, ithCol++, pair.getA());
-							data.setValue(ithRow, ithCol++, pair.getB());
-
-							ithRow++;
-							ithCol = 1;
-						}
-
-						final AbstractDataTable finalData = data;
-						/*-----------------------------------------------------------------*/
-						// Create a callback to be called when the visualization API
-						// has been loaded.
-						Runnable onLoadCallback = new Runnable() 
-						{
-							public void run() {
-								Panel panel = RootPanel.get("databseContainer");
-
-								// Create a pie chart visualization.
-								PieChart pie = new PieChart(finalData, createOptions());
-								BarChart bar = new BarChart(finalData, createOptions());
-								pie.addSelectHandler(createSelectHandler(pie));
-								panel.add(pie);
-								panel.add(bar);
-							}
-						};
-
-						// Load the visualization api, passing the onLoadCallback to be called
-						// when loading is done.
-						VisualizationUtils.loadVisualizationApi(onLoadCallback, CoreChart.PACKAGE);
-						/*-----------------------------------------------------------------*/
-						
-					}
-						});
-			}
-		}
-		DBChartHandler dbChartHandler = new DBChartHandler();
-		getChart.addClickHandler(dbChartHandler);	
-	}
-
-
-	private Options createOptions() {
-		Options options = Options.create();
-		options.setWidth(400);
-		options.setHeight(240);
-		options.setCurveType("cone");
-		options.setTitle("My Daily Activities");
-		return options;
-	}
-	private SelectHandler createSelectHandler(final PieChart chart) {
-		return new SelectHandler() {
-			@Override
-			public void onSelect(SelectEvent event) {
-				String message = "";
-
-				// May be multiple selections.
-				JsArray<Selection> selections = chart.getSelections();
-
-				for (int i = 0; i < selections.length(); i++) {
-					// add a new line for each selection
-					message += i == 0 ? "" : "\n";
-
-					Selection selection = selections.get(i);
-
-					if (selection.isCell()) {
-						// isCell() returns true if a cell has been selected.
-
-						// getRow() returns the row number of the selected cell.
-						int row = selection.getRow();
-						// getColumn() returns the column number of the selected cell.
-						int column = selection.getColumn();
-						message += "cell " + row + ":" + column + " selected";
-					} else if (selection.isRow()) {
-						// isRow() returns true if an entire row has been selected.
-
-						// getRow() returns the row number of the selected row.
-						int row = selection.getRow();
-						message += "row " + row + " selected";
-					} else {
-						// unreachable
-						message += "Pie chart selections should be either row selections or cell selections.";
-						message += "  Other visualizations support column selections as well.";
-					}
-				}
-
-				Window.alert(message);
-			}
-		};
-	}
-
-
+          @Override
+          public void onSuccess(List<Pair<String, Double>> result) {
+            for (Pair<String, Double> pair : result) {
+              Window.alert("categoryName:" + pair.getA());
+              Window.alert("limit:" + pair.getB());
+              categoryName.add(pair.getA());
+              limitList.add(pair.getB());
+            }
+          }
+        });
+    
+      data.addColumn(ColumnType.STRING, "Category");
+      data.addColumn(ColumnType.NUMBER, "Limit");
+    
+      Window.alert("categoryNameSize:" + categoryName.size());
+      data.addRows(categoryName.size());
+      
+      for (int i = 0; i < categoryName.size(); i++) {
+        //Window.alert("categoryName:" + categoryName.get(i));
+        //Window.alert("limit:" + limitList.get(i));
+        data.setValue(i, 0, categoryName.get(i));
+        data.setValue(i, 1, limitList.get(i));
+      }
+    return data;
+  }
 }
