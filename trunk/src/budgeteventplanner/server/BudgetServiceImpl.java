403a44291619915038e05b6e0a3e83d525c392a3
jshanggu@gmail.com <jshanggu@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-12-02 02:34:57 +0000

    Update create Budget, add limit; update budget builder, add limit

--- trunk/src/budgeteventplanner/server/BudgetServiceImpl.java
+++ trunk/src/budgeteventplanner/server/BudgetServiceImpl.java
@@ -35,9 +35,9 @@ public class BudgetServiceImpl extends RemoteServiceServlet implements
 	}
 
 	@Override
-	public Budget createBudget(String eventId, String name) {
+	public Budget createBudget(String eventId, String name, Double limit) {
 		Objectify ofy = ObjectifyService.begin();
-		Budget budget = new Budget.Builder(eventId, name).build();
+		Budget budget = new Budget.Builder(eventId, name, limit).build();
 		ofy.put(budget);
 		return budget;
 	}
@@ -97,16 +97,16 @@ public class BudgetServiceImpl extends RemoteServiceServlet implements
 		return list;
 	}
 	
-	@Override
-	public List<Pair<String, BudgetItem>> getLimitsByBudgetId(String budgetId) {
-	  List<Pair<String, BudgetItem>> list = Lists.newArrayList();
+	public List<Pair<String, Double>> getLimitsByBudgetId(String budgetId) {
+	  List<Pair<String, Double>> list = Lists.newArrayList();
 	  Objectify ofy = ObjectifyService.begin();
 	  Query<BudgetItem> queryBudgetItem = ofy.query(
 	      BudgetItem.class).filter("budgetId", budgetId);
 	    for (BudgetItem item : queryBudgetItem) {
 	      String categoryName = ofy.get(
 	          new Key<Category>(Category.class, item.getCategoryId())).getName();
-	      list.add(new Pair<String, BudgetItem>(categoryName, item));
+	      Double limit = item.getLimit();
+	      list.add(new Pair<String, Double>(categoryName, limit));
 	    }
 	    return list;
 	}
@@ -122,13 +122,5 @@ public class BudgetServiceImpl extends RemoteServiceServlet implements
       budgetList.addAll(queryBudgetItem.list());
     }
     return budgetList;
-  }
-
-  @Override
-  public void updateBudgetItemLimit(String budgetItemId, Double limit) {
-	  Objectify ofy = ObjectifyService.begin();
-	  BudgetItem newBudgetItem = ofy.query(BudgetItem.class).filter("budgetItemId", budgetItemId).get();
-	  newBudgetItem = new BudgetItem.Builder(newBudgetItem).setLimit(limit).build();
-	  ofy.put(newBudgetItem);
   }
 }
