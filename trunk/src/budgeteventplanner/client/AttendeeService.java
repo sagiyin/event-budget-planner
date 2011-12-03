749b85ae97e5bb80dca6cd6c82e121d22b0f1ecc
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-12-03 23:37:07 +0000

    Refactor Attendee Management

--- trunk/src/budgeteventplanner/client/AttendeeService.java
+++ trunk/src/budgeteventplanner/client/AttendeeService.java
@@ -1,9 +1,10 @@
 package budgeteventplanner.client;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import budgeteventplanner.client.entity.Attendee;
 
+import com.google.common.collect.SetMultimap;
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -11,31 +12,30 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 public interface AttendeeService extends RemoteService {
 	Attendee createAttendee(String eventId, String name, String email);
 
-	ArrayList<Attendee> getAttendeeListByOrganizerId(String organizerId);
+	List<Attendee> getAttendeeListByOrganizerId(String organizerId);
 
-	ArrayList<Attendee> getAttendeeListByEventId(String eventId);
+	List<Attendee> getAttendeeListByEventId(String eventId);
 
-	void updateAttendeeInfo(String attendeeId, String name, String email,
-			String jobTitle, String companyName, String address,
-			String phoneNum, Integer status);
+	void updateAttendeeInfo(String attendeeId, String name, String email, String jobTitle,
+			String companyName, String address, String phoneNum, Integer status);
 
 	Integer attendeeLogin(String registrationCode);
 
 	Attendee getAttendeeByAttendeeId(String attendeeId);
 
-	void deleteAttendeeByAttendeeIdList(ArrayList<String> attendeeIdList);
+	void removeAttendeeByAttendeeList(List<Attendee> attendeeList);
 
 	void sendEmail(String attendeeId, Integer status);
 
-	void sendEmailBatch(ArrayList<String> attendeeIdList, Integer status);
+	void sendEmailBatch(List<String> attendeeIdList, Integer status);
 
-	void sendEmailBatchByOrganizer(ArrayList<String> attendeeIdList,
-			Integer status);
+	void sendEmailBatchByOrganizer(List<String> attendeeIdList, Integer status);
 
 	void sendCustomizedEmail(Attendee attendee, String subject, String msgBody);
 
 	void sendCustomizedEmail(String attendeeId, String subject, String msgBody);
 
-	void fillAttendeesInEvent(String eventId, ArrayList<String> attendeeIdList);
+	List<Attendee> fillAttendeesInEvent(String eventId, List<Attendee> attendeeIdList);
 
+	SetMultimap<String, Attendee> getSortedAttendeeList(String organizerId, String excludedEventId);
 }
--- trunk/src/budgeteventplanner/client/AttendeeServiceAsync.java
+++ trunk/src/budgeteventplanner/client/AttendeeServiceAsync.java
@@ -1,41 +1,35 @@
 package budgeteventplanner.client;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import com.google.common.collect.SetMultimap;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import budgeteventplanner.client.entity.Attendee;
 
 public interface AttendeeServiceAsync {
-	void createAttendee(String eventId, String name, String email,
-			AsyncCallback<Attendee> callback);
+	void createAttendee(String eventId, String name, String email, AsyncCallback<Attendee> callback);
 
-	void getAttendeeListByOrganizerId(String organizerId,
-			AsyncCallback<ArrayList<Attendee>> callback);
+	void getAttendeeListByOrganizerId(String organizerId, AsyncCallback<List<Attendee>> callback);
 
-	void getAttendeeListByEventId(String eventId,
-			AsyncCallback<ArrayList<Attendee>> callback);
+	void getAttendeeListByEventId(String eventId, AsyncCallback<List<Attendee>> callback);
 
-	void updateAttendeeInfo(String attendeeId, String name, String email,
-			String jobTitle, String companyName, String address,
-			String phoneNum, Integer status, AsyncCallback<Void> callback);
+	void updateAttendeeInfo(String attendeeId, String name, String email, String jobTitle,
+			String companyName, String address, String phoneNum, Integer status,
+			AsyncCallback<Void> callback);
 
 	void attendeeLogin(String registrationCode, AsyncCallback<Integer> callback);
 
-	void getAttendeeByAttendeeId(String attendeeId,
-			AsyncCallback<Attendee> callback);
+	void getAttendeeByAttendeeId(String attendeeId, AsyncCallback<Attendee> callback);
 
-	void deleteAttendeeByAttendeeIdList(ArrayList<String> attendeeIdList,
+	void removeAttendeeByAttendeeList(List<Attendee> attendeeList, AsyncCallback<Void> callback);
+
+	void sendEmail(String attendeeId, Integer status, AsyncCallback<Void> callback);
+
+	void sendEmailBatch(List<String> attendeeIdList, Integer status, AsyncCallback<Void> callback);
+
+	void sendEmailBatchByOrganizer(List<String> attendeeIdList, Integer status,
 			AsyncCallback<Void> callback);
-
-	void sendEmail(String attendeeId, Integer status,
-			AsyncCallback<Void> callback);
-
-	void sendEmailBatch(ArrayList<String> attendeeIdList, Integer status,
-			AsyncCallback<Void> callback);
-
-	void sendEmailBatchByOrganizer(ArrayList<String> attendeeIdList,
-			Integer status, AsyncCallback<Void> callback);
 
 	void sendCustomizedEmail(Attendee attendee, String subject, String msgBody,
 			AsyncCallback<Void> callback);
@@ -43,6 +37,9 @@ public interface AttendeeServiceAsync {
 	void sendCustomizedEmail(String attendeeId, String subject, String msgBody,
 			AsyncCallback<Void> callback);
 
-	void fillAttendeesInEvent(String eventId, ArrayList<String> attendeeIdList,
-			AsyncCallback<Void> callback);
+	void fillAttendeesInEvent(String eventId, List<Attendee> attendeeIdList,
+			AsyncCallback<List<Attendee>> callback);
+
+	void getSortedAttendeeList(String organizerId, String excludedEventId,
+			AsyncCallback<SetMultimap<String, Attendee>> callback);
 }
