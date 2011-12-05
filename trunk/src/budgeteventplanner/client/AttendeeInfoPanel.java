4d6ee5257de2536758d345276273bc6ce064bf22
yezhuen@gmail.com <yezhuen@gmail.com@52a09e01-30ec-0bfe-7eb2-1485fe15d408>
2011-12-05 03:09:07 +0000

    seems like final version

--- trunk/src/budgeteventplanner/client/AttendeeInfoPanel.java
+++ trunk/src/budgeteventplanner/client/AttendeeInfoPanel.java
@@ -6,106 +6,106 @@ import budgeteventplanner.server.AttendeeServiceImpl;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.DialogBox;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class AttendeeInfoPanel extends Composite {
 	private final AttendeeServiceAsync attendeeService = GWT.create(AttendeeService.class);
-	
+
 	VerticalPanel panelMain = new VerticalPanel();
-	
+	String attendeeId;
+	Button btnUpdate = new Button("Update");
 	public AttendeeInfoPanel(final String attendeeId) {
-		
+		this.attendeeId=attendeeId;
 		// panel info and note area
 		HorizontalPanel hPanelInfo = new HorizontalPanel();
-		//TextArea txtAreaNote = new TextArea();
-		//txtAreaNote.setText("Add a Note");
-		//txtAreaNote.setSize("100%", "100%");
+		// TextArea txtAreaNote = new TextArea();
+		// txtAreaNote.setText("Add a Note");
+		// txtAreaNote.setSize("100%", "100%");
 		// in panelInfo
 		VerticalPanel vPanelInfoLeft = new VerticalPanel();
 		VerticalPanel vPanelInfoRight = new VerticalPanel();
 		hPanelInfo.add(vPanelInfoLeft);
 		hPanelInfo.add(vPanelInfoRight);
 
-		vPanelInfoLeft.add(new Label("Name:"));
+		vPanelInfoLeft.add(new HTML("<h2>Name</h2>"));
 		final TextBox txtBoxName = new TextBox();
 		vPanelInfoLeft.add(txtBoxName);
-		vPanelInfoLeft.add(new Label("Email:"));
+		vPanelInfoLeft.add(new HTML("<h2>Email</h2>"));
 		final TextBox txtBoxEmail = new TextBox();
 		vPanelInfoLeft.add(txtBoxEmail);
-		vPanelInfoLeft.add(new Label("Job Title:"));
+		vPanelInfoLeft.add(new HTML("<h2>Job Title</h2>"));
 		final TextBox txtBoxJobTitle = new TextBox();
 		vPanelInfoLeft.add(txtBoxJobTitle);
 
-		vPanelInfoLeft.add(new Label("Company:"));
+		vPanelInfoLeft.add(new HTML("<h2>Company</h2>"));
 		final TextBox txtBoxCompany = new TextBox();
 		vPanelInfoLeft.add(txtBoxCompany);
 
-		vPanelInfoLeft.add(new Label("Address 1:"));
+		vPanelInfoLeft.add(new HTML("<h2>Address 1</h2>"));
 		final TextBox txtBoxAddress1 = new TextBox();
 		vPanelInfoLeft.add(txtBoxAddress1);
 
-		vPanelInfoRight.add(new Label("Address 2:"));
+		vPanelInfoRight.add(new HTML("<h2>Address 2</h2>"));
 		final TextBox txtBoxAddress2 = new TextBox();
 		vPanelInfoRight.add(txtBoxAddress2);
 
-		vPanelInfoRight.add(new Label("City:"));
+		vPanelInfoRight.add(new HTML("<h2>City</h2>"));
 		final TextBox txtBoxCity = new TextBox();
 		vPanelInfoRight.add(txtBoxCity);
 
-		vPanelInfoRight.add(new Label("State:"));
+		vPanelInfoRight.add(new HTML("<h2>State</h2>"));
 		final TextBox txtBoxState = new TextBox();
 		vPanelInfoRight.add(txtBoxState);
 
-		vPanelInfoRight.add(new Label("Zip:"));
+		vPanelInfoRight.add(new HTML("<h2>Zip</h2>"));
 		final TextBox txtBoxZip = new TextBox();
 		vPanelInfoRight.add(txtBoxZip);
 
-		vPanelInfoRight.add(new Label("Phone Number:"));
+		vPanelInfoRight.add(new HTML("<h2>Phone Number</h2>"));
 		final TextBox txtBoxPhone = new TextBox();
 		vPanelInfoRight.add(txtBoxPhone);
+
 		
-		Button btnUpdate = new Button("Update");
 		HorizontalPanel hpanelUpdate = new HorizontalPanel();
 		hpanelUpdate.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_RIGHT);
-		hpanelUpdate.setSize("100%","100%");
+		hpanelUpdate.setSize("100%", "100%");
 		hpanelUpdate.add(btnUpdate);
-		
-		
-		btnUpdate.addClickHandler(new ClickHandler(){
+
+		btnUpdate.addClickHandler(new ClickHandler() {
 
 			@Override
 			public void onClick(ClickEvent event) {
-				attendeeService.updateAttendeeInfo(
-						attendeeId,
-						txtBoxName.getText(),
-						txtBoxName.getText(),
-						txtBoxJobTitle.getText(),
-						txtBoxCompany.getText(),
-						txtBoxAddress1.getText() + "\n" + txtBoxAddress2.getText()
-								+ "\n" + txtBoxCity.getText() + "\n"
-								+ txtBoxState.getText() + "\n" + txtBoxZip.getText(),
-								txtBoxPhone.getText(),AttendeeServiceImpl.STATUS_NONE, new AsyncCallback<Void>() {
+				attendeeService.updateAttendeeInfo(attendeeId, txtBoxName.getText(),
+						txtBoxEmail.getText(), txtBoxJobTitle.getText(), txtBoxCompany.getText(),
+						txtBoxAddress1.getText() + "\n" + txtBoxAddress2.getText() + "\n"
+								+ txtBoxCity.getText() + "\n" + txtBoxState.getText() + "\n"
+								+ txtBoxZip.getText(), txtBoxPhone.getText(),
+						AttendeeServiceImpl.STATUS_NONE, new AsyncCallback<Void>() {
 
-									@Override
-									public void onFailure(Throwable caught) {
-									}
+							@Override
+							public void onFailure(Throwable caught) {
+								showDialog("Attendee managerMent", "Update Failed");
+							}
 
-									@Override
-									public void onSuccess(Void result) {
-										showDialog("Attendee managerMent","Update Successful");
-									}});
-			}});
+							@Override
+							public void onSuccess(Void result) {
+								showDialog("Attendee managerMent", "Update Successful");
+								btnUpdate.setEnabled(false);
+							}
+						});
+			}
+		});
 		panelMain.add(hPanelInfo);
 		panelMain.add(hpanelUpdate);
 
@@ -128,45 +128,48 @@ public class AttendeeInfoPanel extends Composite {
 			}
 
 		});
-		
+
 		setEnable(false);
 		initWidget(panelMain);
 	}
-	
+
 	public void setEnable(boolean enabled) {
-		HorizontalPanel hPanel = (HorizontalPanel)panelMain.getWidget(0);
-		VerticalPanel vPanelLeft = (VerticalPanel)hPanel.getWidget(0);
-		VerticalPanel vPanelRight = (VerticalPanel)hPanel.getWidget(1);
-		
+		HorizontalPanel hPanel = (HorizontalPanel) panelMain.getWidget(0);
+		VerticalPanel vPanelLeft = (VerticalPanel) hPanel.getWidget(0);
+		VerticalPanel vPanelRight = (VerticalPanel) hPanel.getWidget(1);
+
 		if (enabled) {
 			for (Widget w : vPanelLeft) {
 				if (w instanceof TextBox) {
-					((TextBox) w).setStyleName("enabledTextBox");
+					DOM.setElementAttribute(w.getElement(), "border", "1px");
 					((TextBox) w).setReadOnly(false);
 				}
 			}
 			for (Widget w : vPanelRight) {
 				if (w instanceof TextBox) {
-					((TextBox) w).setStyleName("enabledTextBox");
+					DOM.setElementAttribute(w.getElement(), "border", "1px");
 					((TextBox) w).setReadOnly(false);
 				}
 			}
-			
+			btnUpdate.setEnabled(true);
+
 		} else {
-				for (Widget w : vPanelLeft) {
+			for (Widget w : vPanelLeft) {
 				if (w instanceof TextBox) {
-					((TextBox) w).setStyleName("disabledTextBox");
+					DOM.setElementAttribute(w.getElement(), "border", "0px");
 					((TextBox) w).setReadOnly(true);
 				}
 			}
-				for (Widget w : vPanelRight) {
-					if (w instanceof TextBox) {
-						((TextBox) w).setStyleName("disabledTextBox");
-						((TextBox) w).setReadOnly(true);
-					}
+			for (Widget w : vPanelRight) {
+				if (w instanceof TextBox) {
+					DOM.setElementAttribute(w.getElement(), "border", "0px");
+					((TextBox) w).setReadOnly(true);
 				}
+			}
+			btnUpdate.setEnabled(false);
 		}
 	}
+
 	public static void showDialog(String title, String text) {
 		final DialogBox dialogError = new DialogBox();
 		final Button btnClose = new Button("Close");
@@ -188,4 +191,5 @@ public class AttendeeInfoPanel extends Composite {
 		dialogError.center();
 		btnClose.setFocus(true);
 	}
+	public String getAttendeeId(){return attendeeId;}
 }
